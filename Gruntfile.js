8e982c8558a9f1d834daf0b0400e27454add8398
Claudio Sanches <contato@claudiosmweb.com>
2015-04-21 15:55:32 -0300

    Initial plugin files

---
+++ Gruntfile.js
@@ -0,0 +1,103 @@
+/* jshint node:true */
+module.exports = function( grunt ) {
+'use strict';
+
+	grunt.initConfig({
+
+		// Gets the package vars
+		pkg: grunt.file.readJSON( 'package.json' ),
+		svn_settings: {
+			path: '../../../../wp_plugins/<%= pkg.name %>',
+			tag: '<%= svn_settings.path %>/tags/<%= pkg.version %>',
+			trunk: '<%= svn_settings.path %>/trunk',
+			exclude: [
+				'.git/',
+				'.tx/',
+				'.editorconfig',
+				'.gitignore',
+				'.jshintrc',
+				'node_modules/',
+				'Gruntfile.js',
+				'README.md',
+				'package.json',
+				'*.zip'
+			]
+		},
+
+		// Rsync commands used to take the files to svn repository
+		rsync: {
+			options: {
+				args: ['--verbose'],
+				exclude: '<%= svn_settings.exclude %>',
+				syncDest: true,
+				recursive: true
+			},
+			tag: {
+				options: {
+					src: './',
+					dest: '<%= svn_settings.tag %>'
+				}
+			},
+			trunk: {
+				options: {
+				src: './',
+				dest: '<%= svn_settings.trunk %>'
+				}
+			}
+		},
+
+		// Shell command to commit the new version of the plugin
+		shell: {
+			// Remove delete files.
+			svn_remove: {
+				command: 'svn st | grep \'^!\' | awk \'{print $2}\' | xargs svn --force delete',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Add new files.
+			svn_add: {
+				command: 'svn add --force * --auto-props --parents --depth infinity -q',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			},
+			// Commit the changes.
+			svn_commit: {
+				command: 'svn commit -m "updated the plugin version to <%= pkg.version %>"',
+				options: {
+					stdout: true,
+					stderr: true,
+					execOptions: {
+						cwd: '<%= svn_settings.path %>'
+					}
+				}
+			}
+		}
+	});
+
+	// Load tasks
+	grunt.loadNpmTasks( 'grunt-rsync' );
+	grunt.loadNpmTasks( 'grunt-shell' );
+
+	// Register tasks
+	grunt.registerTask( 'default', [] );
+
+	// Deploy task
+	grunt.registerTask( 'deploy', [
+		'default',
+		'rsync:tag',
+		'rsync:trunk',
+		'shell:svn_remove',
+		'shell:svn_add',
+		'shell:svn_commit'
+	] );
+};
---
+++ woocommerce-itau-shopline.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Itaú Shopline
+ * Plugin URI: https://github.com/claudiosmweb/woocommerce-itau-shopline
+ * Description: Itaú Shopline payment gateway for WooCommerce.
+ * Author: Claudio Sanches
+ * Author URI: https://claudiosmweb.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: woocommerce-itau-shopline
+ * Domain Path: languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'WC_Itau_Shopline' ) ) :
+
+/**
+ * WooCommerce Itaú Shopline main class.
+ */
+class WC_Itau_Shopline {
+
+	/**
+	 * Plugin version.
+	 *
+	 * @var string
+	 */
+	const VERSION = '1.0.0';
+
+	/**
+	 * Instance of this class.
+	 *
+	 * @var object
+	 */
+	protected static $instance = null;
+
+	/**
+	 * Initialize the plugin actions.
+	 */
+	public function __construct() {
+		// Load plugin text domain.
+		add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
+
+		// Checks with WooCommerce and WooCommerce is installed.
+		if ( class_exists( 'WC_Payment_Gateway' ) && defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '2.2', '>=' ) ) {
+			$this->includes();
+
+			// Hook to add Itaú Shopline Gateway to WooCommerce.
+			add_filter( 'woocommerce_payment_gateways', array( $this, 'add_gateway' ) );
+			add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), array( $this, 'plugin_action_links' ) );
+		} else {
+			add_action( 'admin_notices', array( $this, 'dependencies_notices' ) );
+		}
+	}
+
+	/**
+	 * Return an instance of this class.
+	 *
+	 * @return object A single instance of this class.
+	 */
+	public static function get_instance() {
+		// If the single instance hasn't been set, set it now.
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * Load the plugin text domain for translation.
+	 */
+	public function load_plugin_textdomain() {
+		load_plugin_textdomain( 'woocommerce-itau-shopline', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	}
+
+	/**
+	 * Includes.
+	 */
+	private function includes() {
+
+	}
+
+	/**
+	 * Add the gateway to WooCommerce.
+	 *
+	 * @param  array $methods WooCommerce payment methods.
+	 *
+	 * @return array          Payment methods with Itaú Shopline.
+	 */
+	public function add_gateway( $methods ) {
+		$methods[] = 'WC_Itau_Shopline_Gateway';
+
+		return $methods;
+	}
+
+	/**
+	 * Dependencies notices.
+	 */
+	public function dependencies_notices() {
+		include_once 'includes/views/html-notice-woocommerce-missing.php';
+	}
+
+	/**
+	 * Action links.
+	 *
+	 * @param  array $links
+	 *
+	 * @return array
+	 */
+	public function plugin_action_links( $links ) {
+		$plugin_links   = array();
+		$plugin_links[] = '<a href="' . esc_url( admin_url( 'admin.php?page=wc-settings&tab=checkout&section=wc_itau_shopline_gateway' ) ) . '">' . __( 'Settings', 'woocommerce-itau-shopline' ) . '</a>';
+
+		return array_merge( $plugin_links, $links );
+	}
+}
+
+add_action( 'plugins_loaded', array( 'WC_Itau_Shopline', 'get_instance' ) );
+
+endif;
