a5ec34775f9afe2b275e99e7a4a62362614b4dd1
Claudio Sanches <contato@claudiosmweb.com>
2015-09-03 06:04:22 -0300

    Added grunt i18n tools

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,6 +46,49 @@ module.exports = function( grunt ) {
 			}
 		},
 
+		// Make .pot files
+		makepot: {
+			dist: {
+				options: {
+					type: 'wp-plugin',
+					potHeaders: {
+						'report-msgid-bugs-to': 'https://wordpress.org/plugins/woocommerce-domination/',
+						'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+					}
+				}
+			}
+		},
+
+		// Check text domain
+		checktextdomain: {
+			options:{
+				text_domain: '<%= pkg.name %>',
+				keywords: [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				]
+			},
+			files: {
+				src:  [
+					'**/*.php', // Include all files
+					'!node_modules/**' // Exclude node_modules/
+				],
+				expand: true
+			}
+		},
+
 		// Shell command to commit the new version of the plugin
 		shell: {
 			// Remove delete files.
@@ -85,6 +128,8 @@ module.exports = function( grunt ) {
 	});
 
 	// Load tasks
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
 	grunt.loadNpmTasks( 'grunt-rsync' );
 	grunt.loadNpmTasks( 'grunt-shell' );
 
